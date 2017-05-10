478db11544c6a65a34b7acfcb8a154c47f047f4c
13werwolf13 <13werwolf13@mail.ru>
2017-05-10 16:30:19 +0600

    TeamCity test

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@
 int main(int argc, char *argv[])
 {
     //printf("hw");
-    printf("В будущем это будет крутой проект, а пока что это hello world");
+    printf("В будущем это будет крутой проект, а пока что это hello world.");
 }
 //Удачной тебе сборки TeamCity
